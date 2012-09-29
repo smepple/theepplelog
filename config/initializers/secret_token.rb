8245bb5aee7453750da0ebb36c4ee33ef7e4b6ab
Scott Epple <smepple@gmail.com>
2012-09-29 12:01:56 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Theepplelog::Application.config.secret_token = 'c5c82eeaee8964a6dcdfa4518bb84ae353fb76572c25ade964fd84959ba8e5b68cb9d7deafbc799d3dbf7bd2af4ffd8b0d95f432a699a4a1f6df34bd1be703a1'

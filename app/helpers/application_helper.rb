9ab5b28ee0935c3658a81cbf082c42a9610c36f1
Scott Epple <smepple@gmail.com>
2012-09-29 23:17:09 -0400

    Support markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def markdown(text)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, no_intra_emphasis: true, fenced_code_blocks: true, autolink: true)
+    markdown.render(text).html_safe
+  end
 end

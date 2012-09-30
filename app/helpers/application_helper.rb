e19c8f76df4b21c21339a5c39f1d34804f095c80
Scott Epple <smepple@gmail.com>
2012-09-29 23:28:11 -0400

    Better page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 module ApplicationHelper
 
+  def full_title(page_title)
+    base_title = "the epplelog"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+
   def markdown(text)
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, no_intra_emphasis: true, fenced_code_blocks: true, autolink: true)
     markdown.render(text).html_safe
